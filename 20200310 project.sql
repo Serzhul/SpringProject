3d9b8c43b44479052af103a0622ce11d5a07fe62
syanena <wltjdgur94@gmail.com>
2020-03-10 17:59:14 +0900

    20200310 오후05:59 book_main_category랑 book_sub_category추가

--- 20200310 project.sql
+++ 20200310 project.sql
@@ -8,10 +8,20 @@ gender varchar(10) not null,
 auth varchar(10) not null
 );
 
+create table book_main_category(
+main_category_num int,
+main_category_name varchar(30) primary key
+);
+
+create table book_sub_category(
+main_category_num int,
+main_category_name varchar(30) primary key
+);
+
 create table book(
 isbn varchar(50) primary key not null,
-book_m_category varchar(30) not null,
-book_s_category varchar(30) not null,
+book_m_category varchar(30) REFERENCES book_main_category(main_category_name) not null,
+book_s_category varchar(30) REFERENCES book_sub_category(main_category_name)  not null,
 book_subject varchar(50) not null,
 book_writer varchar(20) not null,
 book_pday varchar(50) not null,
@@ -20,6 +30,7 @@ book_filename varchar(50) not null,
 book_filesize int not null,
 book_price int not null
 );
+
 
 create table library(
 id varchar(20) REFERENCES member(id) not null,
@@ -69,15 +80,176 @@ reply_regdate varchar(50) not null
 );
 
 ---------------------------------------------------------------
-alter table book
-add(book_price int);
-
-update book
-set book_price=30000
-where isbn='12345678';
 
 insert into member values('test', '1111', 'test@naver.com', '관리자', '1999-02-02', '남', '0');
 insert into member values('사용자1', '1111', 'test2@naver.com', '바보1', '1999-02-02', '남', '1');
+
+
+
+insert into book_main_category values(
+1,
+'소설'
+);
+insert into book_main_category values(
+,
+'자기 계발'
+);
+insert into book_main_category values(
+3,
+'건강/다이어트'
+);
+insert into book_main_category values(
+4,
+'외국어'
+);
+insert into book_main_category values(
+5,
+'컴튜터/IT'
+);
+insert into book_main_category values(
+6,
+'경영/경제'
+);
+
+insert into book_sub_category values(
+100,
+'소설 전체'
+);
+
+insert into book_sub_category values(
+101,
+'일반'
+);
+
+insert into book_sub_category values(
+102,
+'SF'
+);
+
+insert into book_sub_category values(
+103,
+'추리'
+);
+
+insert into book_sub_category values(
+104,
+'판타지/무협'
+);
+
+insert into book_sub_category values(
+105,
+'로맨스'
+);
+
+insert into book_sub_category values(
+200,
+'자기계발 전체'
+);
+
+insert into book_sub_category values(
+201,
+'성공/삶의 자세'
+);
+
+insert into book_sub_category values(
+202,
+'취업/창업'
+);
+
+insert into book_sub_category values(
+203,
+'설득/화술/협상'
+);
+
+insert into book_sub_category values(
+300,
+'건강/다이어트 전체'
+);
+
+insert into book_sub_category values(
+301,
+'스마일/뷰티'
+);
+
+insert into book_sub_category values(
+302,
+'건강/다이어트'
+);
+
+insert into book_sub_category values(
+303,
+'운동/스포츠'
+);
+
+insert into book_sub_category values(
+400,
+'외국어 전체'
+);
+
+insert into book_sub_category values(
+401,
+'비즈니스 영어'
+);
+
+insert into book_sub_category values(
+402,
+'일반 영어'
+);
+
+insert into book_sub_category values(
+403,
+'제2 외국어'
+);
+
+insert into book_sub_category values(
+500,
+'컴퓨터/IT 전체'
+);
+
+insert into book_sub_category values(
+501,
+'IT비즈니스'
+);
+
+insert into book_sub_category values(
+502,
+'IT자격정'
+);
+
+insert into book_sub_category values(
+503,
+'개발/프로그래밍'
+);
+
+insert into book_sub_category values(
+504,
+'IT해외원서'
+);
+
+insert into book_sub_category values(
+600,
+'경영/경제 전체'
+);
+
+insert into book_sub_category values(
+601,
+'경영 일반'
+);
+
+insert into book_sub_category values(
+602,
+'경제 일반'
+);
+
+insert into book_sub_category values(
+603,
+'마케팅/세일즈'
+);
+
+insert into book_sub_category values(
+604,
+'CEO/리더쉽'
+);
 
 insert into book values(
 '12345678',
@@ -148,12 +320,14 @@ insert into reply values(
 '2020-03-10'
 );
 
---drop table wish;
---drop table review;
---drop table resume;
---drop table reply;
---drop table mycart;
---drop table library;
---drop table book;
---drop table member;
---drop table board;
+drop table wish;
+drop table review;
+drop table resume;
+drop table reply;
+drop table mycart;
+drop table library;
+drop table book;
+drop table board;
+drop table book_main_category;
+drop table book_sub_category;
+drop table member;
